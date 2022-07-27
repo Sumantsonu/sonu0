41c1dea878ec9e3780a5df38d50d46c3a295f5c4
Sumantsonu <sumantkumar.spcet@gmail.com>
2022-07-27 17:58:35 +0530

    ram

---
+++ SAMPLEPROJECT/src/test/java/org/github/githubTest.java
@@ -0,0 +1,20 @@
+package org.github;
+
+import org.testng.annotations.Test;
+
+public class githubTest {
+@Test
+public void GithubTest() {
+	System.out.println("hii everyone");
+}
+@Test
+public void Github1Test() {
+	System.out.println("hii one 1");
+	System.out.println("hii one 6");
+}
+
+@Test
+public void Github2Test() {
+	System.out.println("hii");
+}
+}
